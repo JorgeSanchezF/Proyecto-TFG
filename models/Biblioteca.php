4bde7305f35410835448df6edcc2e65432879d78
JorgeSanchezF <miloyjorge@gmail.com>
2024-05-20 10:23:20 +0200

    implementacion de interfaces arreglada

--- models/Biblioteca.php
+++ models/Biblioteca.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'db/Database.php';
-require_once 'Model.php';
-class Biblioteca implements Model
+
+class Biblioteca
 {
     public function findAll()
     {
@@ -13,11 +13,9 @@ class Biblioteca implements Model
         return $result;
     }
 
-    public function findById($id)
-    {
 
 
-    }
+
     public function findBibliotecaByUsuario($usuarioId)
     {
 
@@ -49,18 +47,9 @@ class Biblioteca implements Model
 
     }
 
-    public function updateById($id, $datos)
-    {
-
-    }
-
-    public function destroyById($id)
-    {
-
-    }
     public function destroyJuegoFromBiblioteca($bibliotecaId, $juegoId)
     {
-        
+
         $query = "DELETE FROM biblioteca_has_juego WHERE biblioteca_id=$bibliotecaId[0] AND juego_id=$juegoId";
         $db = new Database();
         $db = Database::conectar();
