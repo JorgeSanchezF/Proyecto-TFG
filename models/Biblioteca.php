f4a28addf2d998cf5f30a048519165f3e91d4850
JorgeSanchezF <miloyjorge@gmail.com>
2024-05-29 15:52:57 +0200

    Comentarios agregados

--- models/Biblioteca.php
+++ models/Biblioteca.php
@@ -3,6 +3,11 @@ require_once 'db/Database.php';
 
 class Biblioteca
 {
+    /**
+     * Funcion que accede a la bd y recoge todos los juegos almacenados en una biblioteca
+     * 
+     * @return $result
+     */
     public function findAll()
     {
         $query = "SELECT * FROM biblioteca";
@@ -12,7 +17,11 @@ class Biblioteca
         $db = Database::desconectar();
         return $result;
     }
-
+    /**
+     * Funcion que accede a la bd y recoge todos los juegos almacenados en una biblioteca filtrando por usuario
+     * 
+     * @return $result
+     */
     public function findBibliotecaByUsuario($usuarioId)
     {
 
@@ -23,6 +32,11 @@ class Biblioteca
         $db = Database::desconectar();
         return $result;
     }
+    /**
+     * Funcion que accede a la bd y recoge todos los juegos almacenados en una biblioteca filtrando por biblioteca
+     * 
+     * @return $result
+     */
     public function findJuegosByBiblioteca($bibliotecaId)
     {
 
@@ -33,7 +47,11 @@ class Biblioteca
         $db = Database::desconectar();
         return $result;
     }
-
+    /**
+     * Funcion que accede a la bd y almacena en la base de datos un id de juego con id de biblioteca
+     * 
+     * @return 
+     */
     public function store($datos)
     {
         $query = "INSERT INTO biblioteca_has_juego (biblioteca_id, juego_id) VALUE($datos[0],$datos[1]) ";
@@ -43,6 +61,11 @@ class Biblioteca
         $db = Database::desconectar();
 
     }
+    /**
+     * Funcion que accede a la bd y crea una biblioteca con el id de un usuario
+     * 
+     * @return void
+     */
     public function crearBiblioteca($usuario_id)
     {
         $query = "INSERT INTO biblioteca (usuario_id) VALUE($usuario_id) ";
@@ -51,7 +74,11 @@ class Biblioteca
         $db->exec($query);
         $db = Database::desconectar();
     }
-
+    /**
+     * Funcion que accede a la bd elimina de una biblioteca un juego
+     * 
+     * @return void
+     */
     public function destroyJuegoFromBiblioteca($bibliotecaId, $juegoId)
     {
 
@@ -60,6 +87,11 @@ class Biblioteca
         $db = Database::conectar();
         $db->exec($query);
     }
+    /**
+     * Funcion que accede a la bd y elimina una biblioteca
+     * 
+     * @return void
+     */
     public function destroyBiblioteca($usuario_id)
     {
 
